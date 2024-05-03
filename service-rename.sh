3e1396e5d0de9d1590a5ae85b1ac2537761a85b8
Eugene Lebedev <duke.tougu@gmail.com>
2024-05-03 12:23:39 +0300

    Add service-rename.sh script

---
+++ service-rename.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#
+# Rename service name everywhere
+#
+# - Copy vault secrets from one path to another
+# - Create vault policy
+
+if [ $# -ne 3 ]; then
+  echo "Usage: $0 <namespace> <src-service> <dest-service>"
+  exit 1
+fi
+
+namespace=$1
+src_service=$2
+dest_service=$3
+
+./s2v append --vault-src-path=kv/data/${namespace}/${src_service} --vault-dest-path=kv/${namespace}/${dest_service}
+./s2v append --vault-src-path=kv/data/${namespace}/${src_service}-redis --vault-dest-path=kv/${namespace}/${dest_service}
+
+vault policy write ${dest_service} - <<EOF
+path "kv/data/${namespace}/${dest_service}" {
+  capabilities = ["read"]
+}
+EOF
+
+vault write auth/kubernetes/role/${dest_service} policies=${dest_service} bound_service_account_names=${dest_service},vault,vault-secrets-webhook bound_service_account_namespaces=${namespace},vault ttl=24h
+
+echo "---"
+echo "done"
